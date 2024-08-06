e803a61a733b696a540b5295db567caa0454c2b4
DataCodeLiteracy <dataliteracy@icloud.com>
2024-08-06 16:47:46 +0900

    [ ⚙️ | #5 | env ] 개발 단계에서는 image 관련 설정 잠시 보류
    2024.08.08

--- next.config.js
+++ next.config.js
@@ -3,12 +3,12 @@ const { createVanillaExtractPlugin } = require('@vanilla-extract/next-plugin');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: true,
-	images: {
-		loader: 'imgix',
-		path: 'https://pintossmall.mycafe24.com',
-		domains: ['https://pintossmall.mycafe24.com'],
-	},
-	output: 'export',
+	// images: {
+	// 	loader: 'imgix',
+	// 	path: 'https://pintossmall.mycafe24.com',
+	// 	domains: ['https://pintossmall.mycafe24.com'],
+	// },
+	// output: 'export',
 };
 
 const withVanillaExtract = createVanillaExtractPlugin();
