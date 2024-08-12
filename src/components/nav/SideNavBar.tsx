de9ac6929cbae1790ecf47799231f533b3e638c2
DataCodeLiteracy <dataliteracy@icloud.com>
2024-08-12 12:20:11 +0900

    [ ✨ | #12 | feature ] 사이트 정보 사이드바에서 데이터 가져와서 보여주기
    2024.08.12

--- src/components/nav/SideNavBar.tsx
+++ src/components/nav/SideNavBar.tsx
@@ -9,6 +9,7 @@ import Spacing from '@/shared/components/layout/Spacing';
 import Image from 'next/image';
 import { useQuery } from '@tanstack/react-query';
 import { getProductListForSideBar } from '@/app/api/product/getProductList';
+import { getSiteInfo } from '@/app/api/site/getSiteInfo';
 
 const EXCLUDE_PATH = ['/login', '/register'];
 
@@ -18,6 +19,11 @@ const SideNavBar = () => {
 	const { data: products } = useQuery({
 		queryKey: ['sidebarProductList'],
 		queryFn: getProductListForSideBar,
+	});
+
+	const { data: siteInfo } = useQuery({
+		queryKey: ['siteInfo', 1],
+		queryFn: () => getSiteInfo(1),
 	});
 
 	if (EXCLUDE_PATH.includes(path) || path.includes('admin')) return null;
@@ -35,11 +41,11 @@ const SideNavBar = () => {
 			<Flex direction="column">
 				<span className={s.grayText}>대량 구매문의</span>
 				<Spacing margin="11px" />
-				<span className={s.darkBlueText}>1544-4202</span>
+				<span className={s.darkBlueText}>{siteInfo?.data.tel}</span>
 				<Spacing margin="10px" />
 				<Flex>
 					<Image src={KakaoDetailLogo} alt="카카오 상세 로고 이미지" width={19} height={18} />
-					<span className={s.brownText}>카카오톡 m4202</span>
+					<span className={s.brownText}>카카오톡 {siteInfo?.data.kakao}</span>
 				</Flex>
 			</Flex>
 		</div>
