c81b1406cf92ad7478c721ea2a6f913048a69c9c
DataCodeLiteracy <dataliteracy@icloud.com>
2024-08-13 23:39:36 +0900

    [ ♻️  | | refactor ] 상품 상세페이지 옵션 선택 박스 수정
    2024.08.13

--- src/components/product/ProductSelectBox.tsx
+++ src/components/product/ProductSelectBox.tsx
@@ -20,6 +20,9 @@ const ProductSelectBox = ({ product, onSelectCategory }: Props) => {
 	return (
 		<div className={s.selectContainer}>
 			<select className={clsx(s.selectBox, s.mediumGrayText)} onChange={handleChange}>
+				<option selected disabled>
+					상품을 선택해주세요.
+				</option>
 				{product?.priceCategories?.map((category) => (
 					<option key={category.id} value={category.id}>
 						{product.name} {category.name} (카드할인 {product.cardDiscount}% / 휴대폰할인{' '}
